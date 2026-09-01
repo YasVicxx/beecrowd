faf5b81487ef1bf00e0110ae10a8d526f0c41c2b
YasVicxx <yas.vicx@gmail.com>
2026-09-01 16:11:51 -0300

    Add file problem 1004.cs

---
+++ 1004.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int n =int.Parse(Console.ReadLine());
+        int n2 =int.Parse(Console.ReadLine());
+        int PROD = n * n2;
+        Console.WriteLine("PROD = "+ PROD);
+
+    }
+
+}
